eef9958f0a22c9d39a90845e0424174b0fb6770b
udaydadi106 <udaydadi46@gmail.com>
2026-09-04 19:21:39 +0530

    Time: 8 ms (31.81%) | Memory: 46.4 MB (35.41%) - LeetSync

---
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int totalNumbers(int[] digits) {
+     TreeSet<Integer> list=new TreeSet<Integer>();
+     for(int i=0;i<digits.length;i++){
+        if(digits[i]==0){
+            continue;
+        }
+        for(int j=0;j<digits.length;j++){
+        if(i==j){
+            continue;
+        }
+        for(int k=0;k<digits.length;k++){
+            if(k==i||k==j){
+                continue;
+            }
+            if(digits[k]%2!=0){
+                continue;
+            }
+            int digit=digits[i]*100+digits[j]*10+digits[k];
+            list.add(digit);
+        }
+     }   
+     }
+     return list.size();
+    }
+}
