fd1ada65a1bdf9c6d5992a5e3036b4916164d473
udaydadi106 <udaydadi46@gmail.com>
2026-09-13 18:03:52 +0530

    Time: 38 ms (10.72%) | Memory: 47 MB (12.97%) - LeetSync

--- 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.java
+++ 2902-max-pair-sum-in-an-array/max-pair-sum-in-an-array.java
@@ -7,15 +7,6 @@ class Solution {
        a=a/10;
      }
      return max;
-   } 
-   static int sum1(int b){
-    int max1=Integer.MIN_VALUE;
-     while(b>0){
-       int last=b%10;
-       max1=Math.max(max1,last);
-       b=b/10;
-     }
-     return max1;
    }    
     public int maxSum(int[] nums) {
         Arrays.sort(nums);
@@ -23,7 +14,7 @@ class Solution {
         for(int i=nums.length-1;i>0;i--){
             for(int j=i-1;j>=0;j--){
                 int f=sum(nums[i]);
-                int s=sum1(nums[j]);
+                int s=sum(nums[j]);
                 if(f==s){
                 ans=Math.max(ans,nums[i]+nums[j]);
                 }
